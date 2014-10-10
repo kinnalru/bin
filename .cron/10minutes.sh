575d08c008415acdd8b310e212042c3244279cec
Samoilenko Yuri <kinnalru@gmail.com>
2014-10-10 07:40:01 +0400

    Auto commit jerry@jerry.home ip: 192.168.1.151

--- .cron/10minutes.sh
+++ .cron/10minutes.sh
@@ -1,8 +1,10 @@
 #!/bin/env bash
 
+echo " === `date` *** 10minutes" >> ~/.cron/cron.log
 source ~/.profile
 source ~/.cron/env || exit 1
 cd ~/.cron/10minutes || exit 1
 for x in `ls *.sh`; do
-	/bin/sh "$x"
+	echo "  ** run $x:" >> ~/.cron/cron.log
+	/bin/sh "$x" &> ~/.cron.log
 done
