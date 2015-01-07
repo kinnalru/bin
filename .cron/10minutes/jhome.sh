a75b28945ddb536077d5a1d87d7d9563ab009e37
Samoilenko Yuri <kinnalru@gmail.com>
2015-01-07 23:34:09 +0300

    some fix

--- .cron/10minutes/jhome.sh
+++ .cron/10minutes/jhome.sh
@@ -1,3 +1,4 @@
+ssh-add -l | grep -e "`ssh-keygen -lf ~/.ssh/id_rsa.pub | sed s/.pub//`" || ssh-add
 vcsh home commit -am "Auto commit `vcsh home config user.iam`"
 vcsh_merge home "origin/master"
 vcsh home pull
