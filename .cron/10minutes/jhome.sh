9709a0838e7959fbd3da10a6d44f54590f0ff903
Samoilenko Yuri <kinnalru@gmail.com>
2015-01-09 21:27:59 +0300

    gpg-agent replacement for ssh-agent

--- .cron/10minutes/jhome.sh
+++ .cron/10minutes/jhome.sh
@@ -1,4 +1,8 @@
-ssh-add -l | grep -e "`ssh-keygen -lf ~/.ssh/id_rsa.pub | sed s/.pub//`" || ssh-add
+# for ssh-agent only
+#ssh-add -l | grep -e "`ssh-keygen -lf ~/.ssh/id_rsa.pub | sed s/.pub//`" || ssh-add
+
+#for gpg-agent(with ssh-agent emulation) no ssh-add needed
+
 vcsh home commit -am "Auto commit `vcsh home config user.iam`"
 vcsh_merge home "origin/master"
 vcsh home pull
